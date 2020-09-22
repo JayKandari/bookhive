d3cec0f52ec8f0140dcb50ce3380ca0009912ae9
Alphons Jaimon <jaimonalphons@gmail.com>
2020-09-23 02:00:06 +0530

    config_fix-dbConn_add-sessionCookie_fix

--- resources/src/sessionCookie.php
+++ resources/src/sessionCookie.php
@@ -2,9 +2,11 @@
 namespace Session;
 require $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
 use Redirect\Redirect;
+use PDO_CONN\Connection;
 class SessionCookie
 {
-    public $redirect = '';
+    public $redirect;
+    public $dbConn;
     public function __construct()
     {
         $this->redirect = new Redirect;
@@ -14,10 +16,9 @@ class SessionCookie
             $this->redirect->phploc('index.php');
         } else $_SESSION['lastact'] = time();
         if (isset($_SESSION['uid'])) {
-            // $requests = exequery('select type from users where username = ?', 's', $_SESSION['uid']);
-            $requests = '';
-            $row = mysqli_fetch_array($requests);
-            if ($row[0] == 'admin') $_SESSION['admin'] = $_SESSION['uid'];
+            $this->dbConn = new Connection;
+            $row = $this->dbConn->exeQuery('select type from user where uname = ?',$_SESSION['uid']);
+            if ($row[0]['type'] == 'admin') $_SESSION['admin'] = $_SESSION['uid'];
         }
     }
     public function rememberMe()
@@ -25,7 +26,7 @@ class SessionCookie
         $keygen = bin2hex(random_bytes(mt_rand(5, 10))) . $_SESSION['uid'] . bin2hex(random_bytes(mt_rand(1, 5)));
         $key = password_hash($keygen, PASSWORD_ARGON2ID);
         setcookie('remembering', $key, time() + 31556926);
-        // exequery('update users set log=? where username=?', "ss", $keygen, $_SESSION['uid']);
+        $this->dbConn->exeQuery('update user set remember=? where uname=?',$keygen, $_SESSION['uid']);
         unset($keygen);
     }
     public function sessionClear()
