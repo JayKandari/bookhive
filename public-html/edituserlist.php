65c28c68ce8f8423f8e1d9f25b80e95534698559
anjali-rathod <anjali.rathod15010@gmail.com>
2020-09-30 16:48:47 +0530

    responsive code for table

--- public-html/edituserlist.php
+++ public-html/edituserlist.php
@@ -16,19 +16,13 @@ $menu = new Menu(basename(__FILE__), $_SESSION["admin"], $_SESSION["uname"]);
 <html>
 
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href='<?php echo $menu->paths['css'] . "/main.css"; ?>'>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
 </head>
 
 <body>
-<?php
-    $menu->render_header();
-    $menu->render_menu();
-?>
-<div class="main">
-
   <table>
+    <thead>
     <tr>
       <th>User Numner</th>
       <th>User Name</th>
@@ -38,13 +32,16 @@ $menu = new Menu(basename(__FILE__), $_SESSION["admin"], $_SESSION["uname"]);
       <th>Change Type</th>
       <th>Edit Operation</th>
     </tr>
+    </thead>
+  <tbody>
     <?php
-
+    $menu->render_header();
+    $menu->render_menu();
     $k = new user;
     $k->user_info();
     ?>
+  </tbody>
   </table>
-  </div>
   <!-- Include scripts -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
   <script src='<?php echo $menu->paths['js'] . "/main.js" ?>'></script>
