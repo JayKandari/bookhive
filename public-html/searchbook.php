746d179cb9282d16eec82027e8dcfbb2fb51710d
shriaas2898 <aasthashrivastava410@gmail.com>
2020-10-06 10:20:12 +0530

    Added main.css and removed comments

--- public-html/searchbook.php
+++ public-html/searchbook.php
@@ -30,111 +30,108 @@ if (isset($_SESSION["logged_in"])) {
     $menu->render_header();
     $menu->render_menu();
     ?>
-<div class="main_content">
+    <div class="main_content">
 
-    <!-- <div class="container"> -->
-    <div class="main">
-        <div class='header'>
-            <h1>Search Books</h1>
-        </div>
+        <div class="main">
+            <div class='header'>
+                <h1>Search Books</h1>
+            </div>
 
-        <!-- <div class="container search-div"> -->
-        <form action="searchbook.php" method="post" class="row" style="justify-content: center;">
+            <form action="searchbook.php" method="post" class="row" style="justify-content: center;">
+
+                <?php
+
+                if (isset($_SESSION["searchmsg"])) {
+                    echo ('<p id="e">' . $_SESSION["searchmsg"] . "</p>\n");
+                    unset($_SESSION["searchmsg"]);
+                }
+                if (isset($_POST["submit_search"])) {
+                    $title =  $_POST['search_query'];
+                    $author = $_POST['author'];
+                    $catergory = $_POST['category'];
+                } else {
+                    $title =  '';
+                    $author = '';
+                    $catergory = '';
+                }
+                ?>
+
+                <span>
+                    <i class="fa fa-search"></i>
+                    <input type='text' name='search_query' placeholder='Enter title of the book' value='<?php echo $title; ?>'>
+                </span><br>
+                <span>
+                    <i class="fa fa-pen-nib"></i>
+                    <input type='text' name='author' placeholder='Author Name' value='<?php echo $author; ?>'>
+                </span><br>
+                <span>
+                    <i class="fa fa-list"></i>
+                    <input type='text' name='category' placeholder='Category/Genre' value='<?php echo $catergory; ?>'>
+                </span><br>
+                <button name="submit_search"> Search </button>
+
+            </form>
+
 
             <?php
 
-            if (isset($_SESSION["searchmsg"])) {
-                echo ('<p id="e">' . $_SESSION["searchmsg"] . "</p>\n");
-                unset($_SESSION["searchmsg"]);
-            }
+
             if (isset($_POST["submit_search"])) {
-                $title =  $_POST['search_query'];
-                $author = $_POST['author'];
-                $catergory = $_POST['category'];
-            } else {
-                $title =  '';
-                $author = '';
-                $catergory = '';
-            }
+                // Fetch search result from DB
+                $conn = new Connection();
+
+                $query = "SELECT * from Book WHERE title like '%" . $_POST["search_query"] . "%' ";
+                if ($_POST["author"] != "") {
+                    $query = $query . " AND author like '%" . $_POST["author"] . "%'";
+                }
+                if ($_POST["category"] != "") {
+                    $query = $query . " AND category like '%" . $_POST["category"] . "%'";
+                }
+                $result = $conn->exeQuery($query);
+                if ($result) {
+
+
             ?>
-
-            <span>
-                <i class="fa fa-search"></i>
-                <input type='text' name='search_query' placeholder='Enter title of the book' value='<?php echo $title; ?>'>
-            </span><br>
-            <span>
-                <i class="fa fa-pen-nib"></i>
-                <input type='text' name='author' placeholder='Author Name' value='<?php echo $author; ?>'>
-            </span><br>
-            <span>
-                <i class="fa fa-list"></i>
-                <input type='text' name='category' placeholder='Category/Genre' value='<?php echo $catergory; ?>'>
-            </span><br>
-            <button name="submit_search"> Search </button>
-
-        </form>
-
-
-        <?php
-
-
-        if (isset($_POST["submit_search"])) {
-            // Fetch search result from DB
-            $conn = new Connection();
-
-            $query = "SELECT * from Book WHERE title like '%" . $_POST["search_query"] . "%' ";
-            if ($_POST["author"] != "") {
-                $query = $query . " AND author like '%" . $_POST["author"] . "%'";
-            }
-            if ($_POST["category"] != "") {
-                $query = $query . " AND category like '%" . $_POST["category"] . "%'";
-            }
-            $result = $conn->exeQuery($query);
-            if ($result) {
-
-
-        ?>
-                <table legend="2px">
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Author</th>
-                            <th>Category</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        // Display result as table
-                        foreach ($result as $row) {
-                            echo "<tr>
+                    <table legend="2px">
+                        <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Author</th>
+                                <th>Category</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            // Display result as table
+                            foreach ($result as $row) {
+                                echo "<tr>
                      <td data-label='Title'>" . $row['title'] . "</td>
                      <td data-label='Author'>" . $row['author'] . "</td>
                      <td data-label='Category'>" . $row['category'] . "</td><td>";
-                            if (isset($_SESSION["logged_in"])) {
-                                echo '<form method="post">';
-                                echo '<input type="hidden" name="bid" value="' . $row["id"] . '"/>';
-                                echo '<button name="issue">ISSUE BOOK </button>';
-                                echo '</form>';
-                            } else {
-                                echo '<h4><a href="login.php"><button>Login to issue book</button></a></h4>';
+                                if (isset($_SESSION["logged_in"])) {
+                                    echo '<form method="post">';
+                                    echo '<input type="hidden" name="bid" value="' . $row["id"] . '"/>';
+                                    echo '<button name="issue">ISSUE BOOK </button>';
+                                    echo '</form>';
+                                } else {
+                                    echo '<h4><a href="login.php"><button>Login to issue book</button></a></h4>';
+                                }
+                                echo "</td></tr>";
                             }
-                            echo "</td></tr>";
-                        }
 
-                        ?>
-                    </tbody>
-                </table>
-        <?php
+                            ?>
+                        </tbody>
+                    </table>
+            <?php
 
-            } else {
-                $_SESSION["searchmsg"] = "Sorry ! No books found!!";
-                header("LOCATION: searchbook.php");
+                } else {
+                    $_SESSION["searchmsg"] = "Sorry ! No books found!!";
+                    header("LOCATION: searchbook.php");
+                }
             }
-        }
-        ?>
-    </div>
-        <!-- </div> -->
+            ?>
+        </div>
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
     <script src='<?php echo $menu->paths['js'] . "/main.js" ?>'></script>
