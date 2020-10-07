a8b8b604285dd6a02e71c7dbfef8c589a98fb9f7
shriaas2898 <aasthashrivastava410@gmail.com>
2020-10-07 17:34:31 +0530

    Fixes #118: Minor fix in watch sass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,16 @@
-//function defaultTask(cb) {
- // place code for your default task here
- //cb()
-//}
-//exports.default = defaultTask
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-gulp.task('hello', function() {
-	console.log('Hello world!')
-});
 
-//'use strict';
+// Task to compile main.scss to main.css.
 sass.compiler = require('node-sass');
 gulp.task('sass', function () {
-  return gulp.src('./public-html/asset/scss/**/*.scss')
+  return gulp.src('./public-html/asset/scss/**/main.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./public-html/asset/css'));
 });
  
+// Watch task which inovkes `sass` task whenever there is a change in any `.scss` file.
 gulp.task('watch', function () {
-  gulp.watch('./public-html/asset/scss/**/*.scss', ['sass']);
+  gulp.watch('./public-html/asset/scss/**/*.scss', gulp.series('sass'));
 });
-
-
-//const gulp = require("gulp")
-//const sass = require("gulp-sass")
-//const watchSass = require("gulp-watch-sass") 
-//gulp.task("sass:watch", () => watchSass([
-  //"./public-html/asset/**/*.{scss,css}",
-  //"!./public/libs/**/*"
-//])
-  //.pipe(sass())
-  //.pipe(gulp.dest("./public")));
