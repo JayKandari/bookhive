f2d9ffbdfe4e51d0740b6f8c931f517d29641241
Alphons Jaimon <jaimonalphons@gmail.com>
2020-10-10 15:39:47 +0530

    #153 Admin Dashboard UI Fix

--- public-html/admindash.php
+++ public-html/admindash.php
@@ -5,43 +5,25 @@ use template\Menu;
 use src\ProjectConfig;
 use src\SessionCookie;
 use src\book;
+use template\Page;
 
+$page = new Page;
 $session = new SessionCookie;
 $conf = new ProjectConfig();
 
 $session->admincheck();
 
+echo $page->head;
+$page->menu->render_headNav();
 
-$menu = new Menu(basename(__FILE__), $_SESSION["admin"], $_SESSION["uname"]);
+echo '
+      <div class="main_content">
+            <div class="container">
+                  <div class="header">
+                  <h1> All Books Issued</h1>
+</div> </div>';
 
-
-?>
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
-    <link rel="stylesheet" href='<?php echo $conf->config['paths']['css'] . "/main.css"; ?>'>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
-</head>
-
-<body>
-    <?php
-    $menu->render_header();
-    $menu->render_menu();
-    echo '  <div class="main_content">';
-    echo '<h3> All Books Issued</h3>';
-    $k = new book;
-    $row = $k->book_info($_SESSION["uno"], "ad");
-    echo '</div>';
-  ?>
-  
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
-    <script src='<?php echo $menu->paths['js'] . "/main.js" ?>'></script>
-
-</body>
-
-</html>
+$k = new book;
+$row = $k->book_info($_SESSION["uno"], "ad");
+echo '</div>';
+echo $page->foot;
