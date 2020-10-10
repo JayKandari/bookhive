3732deace8229e60042e251a4d2d7af58886f2da
Alphons Jaimon <jaimonalphons@gmail.com>
2020-10-11 01:43:57 +0530

    #155 Edit User UI Fix

--- public-html/edituserlist.php
+++ public-html/edituserlist.php
@@ -1,13 +1,14 @@
 <?php
 require $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
+
 use src\user;
 use src\SessionCookie;
 use template\Menu;
+use template\Page;
 
 $session = new SessionCookie;
 $session->adminCheck();
 
-$menu = new Menu(basename(__FILE__), $_SESSION["admin"], $_SESSION["uname"]);
 $k = new user;
 $mode = "list";
 if (isset($_GET["idu"])) {
@@ -23,44 +24,33 @@ if (isset($_GET["idu"])) {
         $_SESSION["error"] = "No users found";
     }
 }
+$page = new Page;
+
+echo $page->head;
+$page->menu->render_headNav();
 ?>
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href='<?php echo $menu->paths['css'] . "/main.css"; ?>'>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
-</head>
-
-<body>
-    <?php
-    $menu->render_header();
-    $menu->render_menu();
-    ?>
-    <div class="main_content">
-        <div class="header">
-            <h1><?php echo $mode == 'edit' ? 'Edit User' : 'Users' ?></h1>
-        </div>
-        <div class="main">
-
+<div class="main_content">
+    <div class="header">
+        <h1><?php echo $mode == 'edit' ? 'Edit User' : 'Users' ?></h1>
+    </div>
+    <div class="main">
+        <?php
+        if (isset($_SESSION["error"])) {
+            echo ('<p id="e">' . $_SESSION["error"] . "</p>\n");
+            unset($_SESSION["error"]);
+        }
+        if (isset($_SESSION["success"])) {
+            echo ('<p id="g">' . $_SESSION["success"] . "</p>\n");
+            unset($_SESSION["success"]);
+        }
+        $k = new user;
+        $users = $k->getAllUsers();
+        ?>
+        <div <?php echo $mode == "edit" ? ("class='hidden'") : " "; ?>>
             <?php
-            if (isset($_SESSION["error"])) {
-                echo ('<p id="e">' . $_SESSION["error"] . "</p>\n");
-                unset($_SESSION["error"]);
-            }
-            if (isset($_SESSION["success"])) {
-                echo ('<p id="g">' . $_SESSION["success"] . "</p>\n");
-                unset($_SESSION["success"]);
-            }
-            $k = new user;
-            $users = $k->getAllUsers();
-            ?>
-            <div <?php echo $mode == "edit" ? ("class='hidden'") : " "; ?>>
-                <?php
-                echo '<div class="row">';
-                foreach ($users as $row) {
-                    echo '
+            echo '<div class="row">';
+            foreach ($users as $row) {
+                echo '
                     <div class="card main" >
                     <div class="container">
                     <p><b>ID:</b>  ' . $row['id'] . '</p>
@@ -71,60 +61,64 @@ if (isset($_GET["idu"])) {
                     </div>
                     </div>
                     ';
-                }
-                echo '</div>';
-                echo '</div>';
-                ?>
-            </div>
-            <div <?php echo $mode == "list" ? ("class='hidden'") : " "; ?>>
-
-                <form method="post">
-                    <span>
-                        <label for="uname">Name</label>
-                        <input type="text" name="uname" value="<?php echo $name; ?>"></input>
-                    </span><br>
-
-                    <span>
-                        <label for="pass">Password</label>
-                        <input type="password" name="pass" placeholder="Password"></input>
-                    </span><br>
-
-                    <span>
-                        <label for="email">Email</label>
-                        <input type="text" name="email" value="<?php echo $email; ?>"></input>
-                    </span><br>
-
-                    <span>
-                        <label for="admin">Admin</label>
-                        <input type="radio" name="admin" value="admin" <?php echo $role == "admin" ? "checked" : " " ?>>YES </input>
-                        <input type="radio" name="admin" value="user" <?php echo $role == "admin" ? " " : "checked" ?>> NO</input>
-                    </span><br>
-
-                    <button name="submit"> Update Information</button>
-                    <button name="delete_button" class="icon-btn-delete"><i class="fas fa-trash"></i></button>
-                </form>
+            }
+            echo '</div>';
+            echo '</div>';
+            ?>
+        </div>
+        <div <?php echo $mode == "list" ? ("class='hidden'") : " "; ?>>
+            <div class="row">
+                <div class="card main">
+                    <div class="container">
+                        <form method="post">
+                            <p>
+                                <span>
+                                    <label for="uname">Name</label>
+                                    <input type="text" name="uname" value="<?php echo $name; ?>"></input>
+                                </span><br>
+                            </p>
+                            <p>
+                                <span>
+                                    <label for="pass">Password</label>
+                                    <input type="password" name="pass" placeholder="Password"></input>
+                                </span><br>
+                            </p>
+                            <p>
+                                <span>
+                                    <label for="email">Email</label>
+                                    <input type="text" name="email" value="<?php echo $email; ?>"></input>
+                                </span><br>
+                            </p>
+                            <p>
+                                <span>
+                                    <label for="admin">Admin</label>
+                                    <input type="radio" name="admin" value="admin" <?php echo $role == "admin" ? "checked" : " " ?>>YES </input>
+                                    <input type="radio" name="admin" value="user" <?php echo $role == "admin" ? " " : "checked" ?>> NO</input>
+                                </span><br>
+                            </p>
+                            <p>
+                                <button name="submit"> Update Information</button>
+                            </p>
+                            <p>
+                                <button name="delete_button" class="icon-btn-delete"><i class="fas fa-trash"></i></button>
+                            </p>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-    <?php
-    if (isset($_POST["submit"])) {
-        $row = array(
-            "uno" => $uid,
-            "uname" => $_POST["uname"],
-            "email" => $_POST["email"],
-            "pass" => $_POST["pass"],
-            "admin" => $_POST["admin"],
-        );
-        $k->edit($row);
-    }
-    if (isset($_POST["delete_button"])) {
-        $k->delete($uid);
-    }
-    ?>
-    <!-- Include scripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
-    <script src='<?php echo $menu->paths['js'] . "/main.js" ?>'></script>
-
-</body>
-
-</html>
+        <?php
+        if (isset($_POST["submit"])) {
+            $row = array(
+                "uno" => $uid,
+                "uname" => $_POST["uname"],
+                "email" => $_POST["email"],
+                "pass" => $_POST["pass"],
+                "admin" => $_POST["admin"],
+            );
+            $k->edit($row);
+        }
+        if (isset($_POST["delete_button"])) {
+            $k->delete($uid);
+        }
+        echo $page->foot;
